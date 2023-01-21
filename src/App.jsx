6a694a2e686c91a881ed3984f26010d273cf2920
Eric JB <105235872+EB-Joel@users.noreply.github.com>
2023-01-21 12:36:13 -0500

    added components
    Added Music selection, Genre Selection components with a music tracks to test selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Game from './components/game'
+import MusicSelection from './components/MusicSelection'
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -9,6 +10,24 @@ function App() {
   return (
     <div className="App">
       <Game></Game>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <MusicSelection />
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   )
 }
